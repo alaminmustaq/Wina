7bf1e84942619013e5f791a3a4904c8201753829
Al Amin Mustaq <alaminmushtaq321@gmail.com>
2022-04-25 03:09:23 +0600

    init

---
+++ script.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function(){
+    window.addEventListener('scroll', function() {
+      if(y.matches)
+      {
+        if (window.scrollY > 120) {
+          document.getElementById('navbar_top').classList.add('fixed-top');
+          // add padding top to show content behind navbar
+          navbar_height = document.querySelector('.navbar').offsetHeight;
+          document.body.style.paddingTop = navbar_height + 'px';
+        } 
+        else{
+          document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+        } 
+      }
+    });
+    function myFunction(x) {
+      if (x.matches) { // If media query matches
+        document.getElementById('navbar_top').classList.add('fixed-top');
+            // add padding top to show content behind navbar
+            navbar_height = document.querySelector('.navbar').offsetHeight;
+            document.body.style.paddingTop = navbar_height + 'px';
+      }
+      else{
+        document.getElementById('navbar_top').classList.remove('fixed-top');
+           // remove padding top from body
+          document.body.style.paddingTop = '0';
+      }
+    }
+    var y = window.matchMedia("(min-width: 992px)")
+    var x = window.matchMedia("(max-width: 992px)")
+    myFunction(x) // Call listener function at run time
+    x.addListener(myFunction)
+  });
+  
+ 
