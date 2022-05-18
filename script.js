143057d82e8d03dc3c29e84a61292aa52a2338c4
Al Amin Mustaq <alaminmushtaq321@gmail.com>
2022-05-19 01:29:11 +0600

    machine contact page fixed

--- script.js
+++ script.js
@@ -35,6 +35,9 @@ document.addEventListener("DOMContentLoaded", function(){
   });
   
 //  popup starts
+
+  var row_count = 2
+
   const openModelButtons = document.querySelectorAll('[data-model-target]')
   const closeModelButtons = document.querySelectorAll('[data-close-button]')
   const confirmButton = document.querySelectorAll('[data-confirm-button]')
@@ -88,83 +91,240 @@ document.addEventListener("DOMContentLoaded", function(){
     var crane_remot_control = document.getElementById('crane-remot-control-checkbox')
     if( gantry_crane.checked == true)
     {
-      let templet = '<tr><td></td><td><img src="../../../images/granty-crane.jpg" alt="gantry-crane" width="40px"></td><td>Gantry Crane</td><td><span style="color: red;">0.00$</span></td><td><p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p></td><td><div class="counter m-auto"><div class="down">-</div><div class="count"><input type="text" value="01" class="number"></div><div class="up">+</div></div></td><td><button class="remove-button" onclick="deleteRow(this)">Remove</button></td></tr>'
-      table.innerHTML += templet
+
+      var row = table.insertRow(row_count);
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+      var cell6 = row.insertCell(5);
+      var cell7 = row.insertCell(6);
+
+      cell1.innerHTML = "";
+      cell2.innerHTML = '<img src="../../../images/granty-crane.jpg" alt="gantry-crane" width="40px">'
+      cell3.innerHTML = 'Gantry Crane'
+      cell4.innerHTML = '<span style="color: red;">0.00$</span>'
+      cell5.innerHTML = '<p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p>'
+      cell6.innerHTML = '<div class="counter m-auto"><div class="down" onclick="down(this)">-</div><div class="count"><input type="text" value="01" class="number" id="gantry-crane-count"></div><div class="up" onclick="plus(this)">+</div></div>'
+      cell7.innerHTML = '<button class="remove-button" onclick="deleteRow(this)">Remove</button>'
+
+      row_count++
+
       closeModel(model)
     }
     if( double_beam_hooked_gantry_crane.checked == true)
     {
-      let templet = '<tr><td></td><td><img src="../../../images/double-beam-hook-gantry-crane.jpg" alt="double-beam-hook-gantry-crane" width="40px"></td><td>Double Beam Hook Gantry Crane</td><td><span style="color: red;">0.00$</span></td><td><p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p></td><td>blank</td><td><button class="remove-button" onclick="deleteRow(this)">Remove</button></td></tr>'
-      table.innerHTML += templet
+      var row = table.insertRow(row_count)
+      var cell1 = row.insertCell(0)
+      var cell2 = row.insertCell(1)
+      var cell3 = row.insertCell(2)
+      var cell4 = row.insertCell(3)
+      var cell5 = row.insertCell(4)
+      var cell6 = row.insertCell(5)
+      var cell7 = row.insertCell(6)
+
+      cell1.innerHTML = ""
+      cell2.innerHTML = '<img src="../../../images/double-beam-hook-gantry-crane.jpg" alt="double-beam-hook-gantry-crane" width="40px">'
+      cell3.innerHTML = 'Double Beam Hook Gantry Crane'
+      cell4.innerHTML = '<span style="color: red;">0.00$</span>'
+      cell5.innerHTML = '<p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p>'
+      cell6.innerHTML = '<div class="counter m-auto"><div class="down" onclick="down(this)">-</div><div class="count"><input type="text" value="01" class="number" id="double-beam-hook-gantry-crane-count"></div><div class="up" onclick="plus(this)">+</div></div>'
+      cell7.innerHTML = '<button class="remove-button" onclick="deleteRow(this)">Remove</button>'
+
+      row_count++
+
       closeModel(model)
     }
     if( single_beam_crane.checked == true)
     {
-      let templet = '<tr><td></td><td><img src="../../../images/single-beam-crane.jpg" alt="single-beam-crane" width="40px"></td><td>Single Beam Crane</td><td><span style="color: red;">0.00$</span></td><td><p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p></td><td>blank</td><td><button class="remove-button" onclick="deleteRow(this)">Remove</button></td></tr>'
-      table.innerHTML += templet
+      var row = table.insertRow(row_count)
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+      var cell6 = row.insertCell(5);
+      var cell7 = row.insertCell(6);
+
+      cell1.innerHTML = ""
+      cell2.innerHTML = '<img src="../../../images/single-beam-crane.jpg" alt="single-beam-crane" width="40px">'
+      cell3.innerHTML = 'Single Beam Crane'
+      cell4.innerHTML = '<span style="color: red;">0.00$</span>'
+      cell5.innerHTML = '<p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p>'
+      cell6.innerHTML = '<div class="counter m-auto"><div class="down" onclick="down(this)">-</div><div class="count"><input type="text" value="01" class="number" id="single-beam-crane-count"></div><div class="up" onclick="plus(this)">+</div></div>'
+      cell7.innerHTML = '<button class="remove-button" onclick="deleteRow(this)">Remove</button>'
+
+      row_count++
+
       closeModel(model)
     }
     if( double_bridge_crane.checked == true)
     {
-      let templet = '<tr><td></td><td><img src="../../../images/double-beam-crane.jpg" alt="double-bridge-crane" width="40px"></td><td>Double Bridge Crane</td><td><span style="color: red;">0.00$</span></td><td><p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p></td><td>blank</td><td><button class="remove-button" onclick="deleteRow(this)">Remove</button></td></tr>'
-      table.innerHTML += templet
+      var row = table.insertRow(row_count)
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+      var cell6 = row.insertCell(5);
+      var cell7 = row.insertCell(6);
+
+      cell1.innerHTML = ""
+      cell2.innerHTML = '<img src="../../../images/double-beam-crane.jpg" alt="double-bridge-crane" width="40px">'
+      cell3.innerHTML = 'Double Bridge Crane'
+      cell4.innerHTML = '<span style="color: red;">0.00$</span>'
+      cell5.innerHTML = '<p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p>'
+      cell6.innerHTML = '<div class="counter m-auto"><div class="down" onclick="down(this)">-</div><div class="count"><input type="text" value="01" class="number" id="double-beam-crane-count"></div><div class="up" onclick="plus(this)">+</div></div>'
+      cell7.innerHTML = '<button class="remove-button" onclick="deleteRow(this)">Remove</button>'
+
+      row_count++
+
       closeModel(model)
     }
     if( crane_drum_break.checked == true)
     {
-      let templet = '<tr><td></td><td><img src="../../../images/crane-drum-brake .jpg" alt="crane-drum-break" width="40px"></td><td>Crane Drum Brake</td><td><span style="color: red;">0.00$</span></td><td><p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p></td><td>blank</td><td><button class="remove-button" onclick="deleteRow(this)">Remove</button></td></tr>'
-      table.innerHTML += templet
+      var row = table.insertRow(row_count)
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+      var cell6 = row.insertCell(5);
+      var cell7 = row.insertCell(6);
+
+      cell1.innerHTML = ""
+      cell2.innerHTML = '<img src="../../../images/crane-drum-brake .jpg" alt="crane-drum-break" width="40px">'
+      cell3.innerHTML = 'Crane Drum Brake'
+      cell4.innerHTML = '<span style="color: red;">0.00$</span>'
+      cell5.innerHTML = '<p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p>'
+      cell6.innerHTML = '<div class="counter m-auto"><div class="down" onclick="down(this)">-</div><div class="count"><input type="text" value="01" class="number" id="crane-drum-brake-count"></div><div class="up" onclick="plus(this)">+</div></div>'
+      cell7.innerHTML = '<button class="remove-button" onclick="deleteRow(this)">Remove</button>'
+
+      row_count++
+
       closeModel(model)
     }
     if( clamshell_grab.checked == true)
     {
-      let templet = '<tr><td></td><td><img src="../../../images/clamshell-grab.jpg" alt="clamshell-grab" width="40px"></td><td>Clamshell Grab</td><td><span style="color: red;">0.00$</span></td><td><p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p></td><td>blank</td><td><button class="remove-button" onclick="deleteRow(this)">Remove</button></td></tr>'
-      table.innerHTML += templet
+      var row = table.insertRow(row_count)
+      var cell1 = row.insertCell(0)
+      var cell2 = row.insertCell(1)
+      var cell3 = row.insertCell(2)
+      var cell4 = row.insertCell(3)
+      var cell5 = row.insertCell(4)
+      var cell6 = row.insertCell(5)
+      var cell7 = row.insertCell(6)
+
+      cell1.innerHTML = ""
+      cell2.innerHTML = '<img src="../../../images/clamshell-grab.jpg" alt="clamshell-grab" width="40px">'
+      cell3.innerHTML = 'Clamshell Grab'
+      cell4.innerHTML = '<span style="color: red;">0.00$</span>'
+      cell5.innerHTML = '<p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p>'
+      cell6.innerHTML = '<div class="counter m-auto"><div class="down" onclick="down(this)">-</div><div class="count"><input type="text" value="01" class="number" id="clamshell-grab-count"></div><div class="up" onclick="plus(this)">+</div></div>'
+      cell7.innerHTML = '<button class="remove-button" onclick="deleteRow(this)">Remove</button>'
+
+      row_count++
+
       closeModel(model)
     }
     if( wire_roop_electric_hoist.checked == true)
     {
-      let templet = '<tr><td></td><td><img src="../../../images/wire-rope-electric-hoist.jpg" alt="wire-rope-electric-hoist" width="40px"></td><td>Wire Rope Electric Hoist</td><td><span style="color: red;">0.00$</span></td><td><p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p></td><td>blank</td><td><button class="remove-button" onclick="deleteRow(this)">Remove</button></td></tr>'
-      table.innerHTML += templet
+      var row = table.insertRow(row_count)
+      var cell1 = row.insertCell(0)
+      var cell2 = row.insertCell(1)
+      var cell3 = row.insertCell(2)
+      var cell4 = row.insertCell(3)
+      var cell5 = row.insertCell(4)
+      var cell6 = row.insertCell(5)
+      var cell7 = row.insertCell(6)
+
+      cell1.innerHTML = ""
+      cell2.innerHTML = '<img src="../../../images/wire-rope-electric-hoist.jpg" alt="wire-rope-electric-hoist" width="40px">'
+      cell3.innerHTML = 'Wire Rope Electric Hoist'
+      cell4.innerHTML = '<span style="color: red;">0.00$</span>'
+      cell5.innerHTML = '<p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p>'
+      cell6.innerHTML = '<div class="counter m-auto"><div class="down" onclick="down(this)">-</div><div class="count"><input type="text" value="01" class="number" id="wire-rope-electric-hoist-count"></div><div class="up" onclick="plus(this)">+</div></div>'
+      cell7.innerHTML = '<button class="remove-button" onclick="deleteRow(this)">Remove</button>'
+
+      row_count++
+
       closeModel(model)
     }
     if( crane_wheels.checked == true)
     {
-      let templet = '<tr><td></td><td><img src="../../../images/cran-wheels.jpg" alt="crane-wheels" width="40px"></td><td>Crane Wheels</td><td><span style="color: red;">0.00$</span></td><td><p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p></td><td>blank</td><td><button class="remove-button" onclick="deleteRow(this)">Remove</button></td></tr>'
-      table.innerHTML += templet
+      var row = table.insertRow(row_count)
+      var cell1 = row.insertCell(0)
+      var cell2 = row.insertCell(1)
+      var cell3 = row.insertCell(2)
+      var cell4 = row.insertCell(3)
+      var cell5 = row.insertCell(4)
+      var cell6 = row.insertCell(5)
+      var cell7 = row.insertCell(6)
+
+      cell1.innerHTML = ""
+      cell2.innerHTML = '<img src="../../../images/cran-wheels.jpg" alt="crane-wheels" width="40px">'
+      cell3.innerHTML = 'Crane Wheels'
+      cell4.innerHTML = '<span style="color: red;">0.00$</span>'
+      cell5.innerHTML = '<p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p>'
+      cell6.innerHTML = '<div class="counter m-auto"><div class="down" onclick="down(this)">-</div><div class="count"><input type="text" value="01" class="number" id="cran-wheels-count"></div><div class="up" onclick="plus(this)">+</div></div>'
+      cell7.innerHTML = '<button class="remove-button" onclick="deleteRow(this)">Remove</button>'
+
+      row_count++
+    
       closeModel(model)
     }
     if( crane_remot_control.checked == true)
     {
-      let templet = '<tr><td></td><td><img src="../../../images/remot-control.jpg" alt="remot-control" width="40px"></td><td>Crane Remot Control</td><td><span style="color: red;">0.00$</span></td><td><p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p></td><td>blank</td><td><button class="remove-button" onclick="deleteRow(this)">Remove</button></td></tr>'
-      table.innerHTML += templet
+      var row = table.insertRow(row_count)
+      var cell1 = row.insertCell(0)
+      var cell2 = row.insertCell(1)
+      var cell3 = row.insertCell(2)
+      var cell4 = row.insertCell(3)
+      var cell5 = row.insertCell(4)
+      var cell6 = row.insertCell(5)
+      var cell7 = row.insertCell(6)
+
+      cell1.innerHTML = ""
+      cell2.innerHTML = '<img src="../../../images/remot-control.jpg" alt="remot-control" width="40px">'
+      cell3.innerHTML = 'Crane Remot Control'
+      cell4.innerHTML = '<span style="color: red;">0.00$</span>'
+      cell5.innerHTML = '<p class="border m-0" style="color: rgb(202, 200, 200);">Please en</p>'
+      cell6.innerHTML = '<div class="counter m-auto"><div class="down" onclick="down(this)">-</div><div class="count"><input type="text" value="01" class="number" id="remot-control-count"></div><div class="up" onclick="plus(this)">+</div></div>'
+      cell7.innerHTML = '<button class="remove-button" onclick="deleteRow(this)">Remove</button>'
+
+      row_count++
+
       closeModel(model)
     }
   }
 //  popup ends
 // inquiry_table starts
-const plus = document.querySelector('.up')
-const down = document.querySelector('.down')
-const count = document.querySelector('.number')
+
+var count = document.querySelector('.number')
 let a = count.value
 
-plus.addEventListener("click",()=>{
+function plus(btn){
+  count = btn.parentNode.querySelector('.number')
   a = count.value
   a++
   a = (a<10) ? "0" + a : a 
   count.value = a
-})
-down.addEventListener("click",()=>{
+  console.log(count.id)
+}
+function down(btn){
+  count = btn.parentNode.querySelector('.number')
   a = count.value
   if(count.value > 1){
     a--
     a = (a<10) ? "0" + a : a 
     count.value = a
   }
-})
+}
 
 function deleteRow(btn) {
   var row = btn.parentNode.parentNode;
   row.parentNode.removeChild(row);
+  row_count--
 }
 // inquiry_table ends
